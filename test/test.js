8709b1b44e16e1c9107609506fc2e2c6fb2653b6
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-11-17 15:02:15 +0000

    Add basic test for 'register'

--- test/test.js
+++ test/test.js
@@ -1,7 +1,23 @@
 var mercurius = require('../index.js');
+var request = require('supertest');
+var assert = require('assert');
 
 describe('mercurius', function() {
   before(function() {
     return mercurius.ready;
+  });
+
+  it('successfully registers users', function(done) {
+    request(mercurius.app)
+      .post('/register')
+      .send({
+        endpoint: 'endpoint',
+        key: 'key'
+      })
+      .expect(function(res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text.length, 64);
+      })
+      .end(done);
   });
 });
