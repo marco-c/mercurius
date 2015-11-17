09b629cbbdacc03d724ccad6d893c667b952a4be
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-11-17 16:29:01 +0000

    Test successfully sending a notification (using nock to mock a push service)

--- test/test.js
+++ test/test.js
@@ -1,10 +1,24 @@
 var mercurius = require('../index.js');
 var request = require('supertest');
 var assert = require('assert');
+var nock = require('nock');
 
 describe('mercurius', function() {
-  before(function() {
-    return mercurius.ready;
+  var token;
+
+  before(function(done) {
+    mercurius.ready.then(function() {
+      request(mercurius.app)
+        .post('/register')
+        .send({
+          endpoint: 'https://localhost:50005',
+          key: 'key',
+        })
+        .expect(function(res) {
+          token = res.text;
+        })
+        .end(done);
+    });
   });
 
   it('successfully registers users', function(done) {
@@ -28,6 +42,19 @@ describe('mercurius', function() {
         token: 'token_inesistente',
       })
       .expect(404, done);
+  });
+
+  it('sends a notification to a registered user', function(done) {
+    nock('https://localhost:50005')
+    .post('/')
+    .reply(201);
+
+    request(mercurius.app)
+      .post('/notify')
+      .send({
+        token: token,
+      })
+      .expect(200, done);
   });
 
   it('sends 404 on `updateRegistration` when a registration doesn\'t exist', function(done) {
