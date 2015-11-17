ab1af22eb4b02f841773480257a1f0d0a4648d13
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-11-17 14:02:52 +0000

    Force SSL

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next) {
+  var host = req.get('Host');
+  var localhost = 'localhost';
+
+  if (host.substring(0, localhost.length) !== localhost) {
+    // https://developer.mozilla.org/en-US/docs/Web/Security/HTTP_strict_transport_security
+    res.header('Strict-Transport-Security', 'max-age=15768000');
+    // https://github.com/rangle/force-ssl-heroku/blob/master/force-ssl-heroku.js
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect('https://' + host + req.url);
+    }
+  }
+  return next();
+});
+
 var port = process.env.PORT || 3003;
 var ready = new Promise(function(resolve, reject) {
   app.listen(port, function(err) {
