160fa12694099faa389bc0202458f356a9a23622
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-11-18 15:16:16 +0000

    Show notification using the parameters from the push message payload. Fixes #7

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,5 +1,10 @@
 self.addEventListener('push', function(event) {
-  event.waitUntil(self.registration.showNotification('Mercurius', {
-    body: 'Notification!',
+  var data = event.data ? event.data.json() : null;
+
+  var title = data ? data.title : 'Mercurius';
+  var body = data ? data.body : 'Notification';
+
+  event.waitUntil(self.registration.showNotification(title, {
+    body: body,
   }));
 });
