1f3f557dd02cd1444ac4c58e8fb589c0b5a702f8
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-11-18 13:32:13 +0000

    Make page human-readable

--- static/index.js
+++ static/index.js
@@ -1,28 +1,34 @@
-navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-  return registration.pushManager.getSubscription().then(function(subscription) {
-    if (subscription) {
-      return subscription;
-    }
+var registrationPromise = navigator.serviceWorker.register('service-worker.js');
 
-    return registration.pushManager.subscribe({ userVisibleOnly: true }).then(function(newSubscription) {
-      return newSubscription;
+function register() {
+  registrationPromise.then(function(registration) {
+    return registration.pushManager.getSubscription().then(function(subscription) {
+      if (subscription) {
+        return subscription;
+      }
+
+      return registration.pushManager.subscribe({ userVisibleOnly: true }).then(function(newSubscription) {
+        return newSubscription;
+      });
+    });
+  }).then(function(subscription) {
+    var key = subscription.getKey ? subscription.getKey('p256dh') : '';
+
+    fetch('./register', {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        endpoint: subscription.endpoint,
+        key: key ? btoa(String.fromCharCode.apply(null, new Uint8Array(key))) : '',
+      }),
+    }).then(function(response) {
+      response.text().then(function(token) {
+        document.getElementById('token').textContent = 'Your token is: ' + token;
+      });
     });
   });
-}).then(function(subscription) {
-  var key = subscription.getKey ? subscription.getKey('p256dh') : '';
+}
 
-  fetch('./register', {
-    method: 'post',
-    headers: {
-      'Content-type': 'application/json'
-    },
-    body: JSON.stringify({
-      endpoint: subscription.endpoint,
-      key: key ? btoa(String.fromCharCode.apply(null, new Uint8Array(key))) : '',
-    }),
-  }).then(function(response) {
-    response.text().then(function(token) {
-      window.alert('Your token is: ' + token);
-    });
-  });
-});
+document.getElementById('register').onclick = register;
