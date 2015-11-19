1be025b82d3be2f10f102caee29c8a74e6e4392d
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-11-19 16:38:22 +0000

    Keep user subscription updated. Fixes #4

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,3 +1,5 @@
+importScripts('localforage.min.js');
+
 self.addEventListener('push', function(event) {
   var data = event.data ? event.data.json() : null;
 
@@ -7,4 +9,30 @@ self.addEventListener('push', function(event) {
   event.waitUntil(self.registration.showNotification(title, {
     body: body,
   }));
+});
+
+self.addEventListener('pushsubscriptionchange', function(event) {
+  event.waitUntil(
+    localforage.getItem('token').then(function(token) {
+      if (!token) {
+        return;
+      }
+
+      return self.registration.pushManager.subscribe({ userVisibleOnly: true }).then(function(subscription) {
+        var key = subscription.getKey ? subscription.getKey('p256dh') : '';
+
+        return fetch('./updateRegistration', {
+          method: 'post',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify({
+            token: token,
+            endpoint: subscription.endpoint,
+            key: key ? btoa(String.fromCharCode.apply(null, new Uint8Array(key))) : '',
+          }),
+        });
+      });
+    })
+  );
 });
