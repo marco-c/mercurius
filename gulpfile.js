5c80a1e77dafff72f456db2074406c8512351902
Marco Castelluccio <mcastelluccio@mozilla.com>
2015-12-18 16:09:24 +0000

    Don't copy unused lib/canvas.js file

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,6 @@ gulp.task('default', ['clean', 'lint'], function() {
     .src(['static/*',
           'node_modules/localforage/dist/localforage.min.js',
           'node_modules/quagga/dist/quagga.min.js',
-          'node_modules/bwip-js/bwip.js', 'node_modules/bwip-js/bwipp/code128.js', 'node_modules/bwip-js/bwipp/raiseerror.js', 'node_modules/bwip-js/bwipp/renlinear.js', 'node_modules/bwip-js/lib/canvas.js', 'node_modules/bwip-js/Inconsolata.otf', 'node_modules/bwip-js/freetype.js', 'node_modules/bwip-js/freetype.js.mem'])
+          'node_modules/bwip-js/bwip.js', 'node_modules/bwip-js/bwipp/code128.js', 'node_modules/bwip-js/bwipp/raiseerror.js', 'node_modules/bwip-js/bwipp/renlinear.js', 'node_modules/bwip-js/Inconsolata.otf', 'node_modules/bwip-js/freetype.js', 'node_modules/bwip-js/freetype.js.mem'])
     .pipe(gulp.dest('dist'));
 });
