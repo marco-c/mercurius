b02bf62700066fe98523f42b788217745f30e783
Marco Castelluccio <mcastelluccio@mozilla.com>
2015-12-19 21:55:44 +0000

    Clarify gulp build task

--- gulpfile.js
+++ gulpfile.js
@@ -18,9 +18,14 @@ gulp.task('lint', function() {
 
 gulp.task('default', ['clean', 'lint'], function() {
   return gulp
-    .src(['static/*',
-          'node_modules/localforage/dist/localforage.min.js',
-          'node_modules/quagga/dist/quagga.min.js',
-          'node_modules/bwip-js/bwip.js', 'node_modules/bwip-js/bwipp/code128.js', 'node_modules/bwip-js/bwipp/raiseerror.js', 'node_modules/bwip-js/bwipp/renlinear.js', 'node_modules/bwip-js/Inconsolata.otf', 'node_modules/bwip-js/freetype.js', 'node_modules/bwip-js/freetype.js.mem'])
+    .src([
+      'static/*',
+      // localForage
+      'node_modules/localforage/dist/localforage.min.js',
+      // QuaggaJS
+      'node_modules/quagga/dist/quagga.min.js',
+      // bwip-js
+      'node_modules/bwip-js/bwip.js', 'node_modules/bwip-js/bwipp/code128.js', 'node_modules/bwip-js/bwipp/raiseerror.js', 'node_modules/bwip-js/bwipp/renlinear.js', 'node_modules/bwip-js/Inconsolata.otf', 'node_modules/bwip-js/freetype.js', 'node_modules/bwip-js/freetype.js.mem'
+    ])
     .pipe(gulp.dest('dist'));
 });
