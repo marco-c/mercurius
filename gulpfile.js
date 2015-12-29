adf09c1bd49097545acde24cd8024d6112221725
Marco Castelluccio <mcastelluccio@mozilla.com>
2015-12-29 19:53:28 +0100

    Don't copy the unused Inconsolata font in the dist/ directory

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('default', ['clean', 'lint'], function() {
       // QuaggaJS
       'node_modules/quagga/dist/quagga.min.js',
       // bwip-js
-      'node_modules/bwip-js/bwip.js', 'node_modules/bwip-js/bwipp/code128.js', 'node_modules/bwip-js/bwipp/raiseerror.js', 'node_modules/bwip-js/bwipp/renlinear.js', 'node_modules/bwip-js/Inconsolata.otf', 'node_modules/bwip-js/freetype.js', 'node_modules/bwip-js/freetype.js.mem'
+      'node_modules/bwip-js/bwip.js', 'node_modules/bwip-js/bwipp/code128.js', 'node_modules/bwip-js/bwipp/raiseerror.js', 'node_modules/bwip-js/bwipp/renlinear.js', 'node_modules/bwip-js/freetype.js', 'node_modules/bwip-js/freetype.js.mem',
     ])
     .pipe(gulp.dest('dist'));
 });
