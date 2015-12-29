9c98b78916fceb5ab213da8f805f1289bb4b28d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2015-12-29 19:10:46 +0100

    Use a global afterEach to test that all nock requests have been made. Fixes #66

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,10 @@
 var request = require('supertest');
+var nock = require('nock');
+var assert = require('assert');
+
+afterEach(function() {
+  assert(nock.isDone(), 'All requests have been made. Pending: ' + nock.pendingMocks());
+});
 
 module.exports = {
   register: function(app, machine, endpoint, key, existingToken) {
