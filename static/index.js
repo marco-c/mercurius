cac29ae5500bb6381afa7efad9dce4ea98a053fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2015-12-30 16:14:29 +0100

    Register 'updatefound' event listener and ask user if he wants to update

--- static/index.js
+++ static/index.js
@@ -278,8 +278,34 @@ function toggleMachineClientNotification(token, machineId, client) {
   });
 }
 
+function updateFound() {
+  var installingWorker = this.installing;
+
+  // Wait for the new service worker to be installed before prompting to update.
+  installingWorker.addEventListener('statechange', function() {
+    switch (installingWorker.state) {
+      case 'installed':
+        // Only show the prompt if there is currently a controller so it is not
+        // shown on first load.
+        if (navigator.serviceWorker.controller &&
+            window.confirm('An updated version of this page is available, would you like to update?')) {
+          window.location.reload();
+          return;
+        }
+        break;
+
+      case 'redundant':
+        console.error('The installing service worker became redundant.');
+        break;
+    }
+  });
+}
+
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('offline-worker.js');
+  navigator.serviceWorker.register('offline-worker.js')
+  .then(function(registration) {
+    registration.addEventListener('updatefound', updateFound);
+  });
 } else {
   showSection('unsupported');
 }
