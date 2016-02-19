989f95f7c86ac17132029c57180773dede974d3d
Valentin Brosseau <c4software@gmail.com>
2016-02-20 00:39:57 +0100

    Let the Webserver manage the https redirection

--- index.js
+++ index.js
@@ -16,15 +16,6 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   var host = req.get('Host');
-
-  if (host.indexOf('localhost') !== 0 && host.indexOf('127.0.0.1') !== 0) {
-    // https://developer.mozilla.org/en-US/docs/Web/Security/HTTP_strict_transport_security
-    res.header('Strict-Transport-Security', 'max-age=15768000');
-    // https://github.com/rangle/force-ssl-heroku/blob/master/force-ssl-heroku.js
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect('https://' + host + req.url);
-    }
-  }
   return next();
 });
 
